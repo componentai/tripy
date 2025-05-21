ba97c7502bd8725a20ab6835e55d23dfef1da01a
Rasmus Pallisgaard <rpa@component-ai.dk>
2025-05-21 20:46:58 +0200

    third time's the charm

--- setup.py
+++ setup.py
@@ -2,22 +2,5 @@ from setuptools import setup
 
 setup(
     name="tripy",
-    url="https://github.com/linuxlewis/tripy",
-    author="Sam Bolgert",
-    author_email="sbolgert@gmail.com",
-    description="Simple polygon triangulation",
-    license="MIT",
-    py_modules=["tripy"],
     include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
 )
